6529be649f61096e08dfb9e931bc2befb4e8dfc4
milton bravo <miltonbravo96@gmail.com>
2020-08-21 23:30:56 -0500

    obtener datos de la db

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,29 +1,39 @@
-const { Router } = require('express');
+const { Router } = require("express");
+const { Exception } = require("handlebars");
 const router = Router();
+const movie = require("../controllers/movie.controller");
 
-router.get('/', (req, res) => {
-    res.render('index');
+router.get("/", async (req, res) => {
+  try {
+    let movies = await movie.indexMovies();
+    res.render("index", { movies: movies });
+  } catch (error) {
+    error.code = 500;
+    error.description = "Error al obtener los datos";
+    res.render("error", { error });
+  }
 });
 
-router.get('/testing', (req, res) => {
-    const pelis = [
-        {
-            "name": "primera",
-            "desc": "primera pelicula"
-        },
-        {
-            "name": "segunda",
-            "desc": "segunda pelicula"
-        },
-        {
-            "name": "tercera",
-            "desc": "tercera pelicula"
-        }];
-    const ciudad = {
-        "nombre": "aguascalienes",
-        "poblacion": 100
-    };
-    res.render('testing', { "peliculas": pelis, "city": ciudad });
+router.get("/testing", (req, res) => {
+  const pelis = [
+    {
+      name: "primera",
+      desc: "primera pelicula",
+    },
+    {
+      name: "segunda",
+      desc: "segunda pelicula",
+    },
+    {
+      name: "tercera",
+      desc: "tercera pelicula",
+    },
+  ];
+  const ciudad = {
+    nombre: "aguascalienes",
+    poblacion: 100,
+  };
+  res.render("testing", { peliculas: pelis, city: ciudad });
 });
 
 module.exports = router;
