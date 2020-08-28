56ead5f3f7aa248db8ed2091e683207e275904c3
milton bravo <miltonbravo96@gmail.com>
2020-08-27 22:10:27 -0500

    actualizadas rutas de archivos estaticos

--- src/server.js
+++ src/server.js
@@ -28,7 +28,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(require('./routes/index.routes'));
 
 // Archivos estaticos
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(favicon(path.join(__dirname, '/public', 'favicon.ico')));
 
 module.exports = app;
