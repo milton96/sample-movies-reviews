1ca88ac71a26a7d4cd52143483048f4b7e7f6a37
milton bravo <miltonbravo96@gmail.com>
2020-08-27 23:49:45 -0500

    Aumento al número de registros obtenidos

--- src/controllers/movie.controller.js
+++ src/controllers/movie.controller.js
@@ -1,9 +1,15 @@
+const moongose = require('mongoose');
 const movieController = {};
-const Movie = require('../models/Movie');
+const Movie = require("../models/Movie");
 
 movieController.indexMovies = async () => {
-    return await Movie.find({ poster: { $exists: true } }).limit(5).lean();
-}
+    return await Movie.find({ poster: { $exists: true } })
+        .limit(50)
+        .lean();
+};
 
+movieController.getMovie = async (movieId) => {
+    return await Movie.find({ _id: moongose.Types.ObjectId(movieId) });
+};
 
 module.exports = movieController;
